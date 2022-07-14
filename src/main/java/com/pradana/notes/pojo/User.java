e4185732ddfc5c6fae02779828aa4297de646070
pradana4648 <wakwaujaim@gmail.com>
2022-07-14 22:10:16 +0700

    refactor(User): removed getter/setter for field id

--- src/main/java/com/pradana/notes/pojo/User.java
+++ src/main/java/com/pradana/notes/pojo/User.java
@@ -5,15 +5,24 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
 @Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Getter
+    @Setter
     private String username;
+
+    @Getter
+    @Setter
     private String email;
+
+    @Getter
+    @Setter
     private String password;
 }
