4c6fee3bf492551d7757f3712b1f2d1da2a58da9
pradana4648 <wakwaujaim@gmail.com>
2022-07-14 20:59:29 +0700

    chore: added tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   mode: process.env.NODE_ENV ? "jit" : undefined,
   content: ["./src/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {},
   },
   plugins: [],
