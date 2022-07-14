3bc799757fb9b6c691d866c2db6d6f1790931064
pradana4648 <wakwaujaim@gmail.com>
2022-07-14 22:09:33 +0700

    refactor: renamed UserDto to LoginDto

--- src/main/java/com/pradana/notes/dto/LoginDto.java
+++ src/main/java/com/pradana/notes/dto/LoginDto.java
@@ -3,17 +3,11 @@ package com.pradana.notes.dto;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 import lombok.Data;
 
 @Data
-public class UserDto {
-    @NotNull(message = "Nama harus di isi")
-    @NotBlank(message = "Nama tidak boleh kosong")
-    @Size(min = 4, message = "Nama hanya boleh di isi min 4 karakter")
-    private String username;
-
+public class LoginDto {
     @NotNull(message = "Email harus di isi")
     @NotBlank(message = "Email tidak boleh kosong")
     @Email(message = "Email tidak valid", regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
@@ -21,6 +15,5 @@ public class UserDto {
 
     @NotNull(message = "Password harus di isi")
     @NotBlank(message = "Password tidak boleh kosong")
-    @Size(min = 8, message = "Password minimal 8 karakter")
     private String password;
 }
