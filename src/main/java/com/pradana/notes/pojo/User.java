eb1ccb921af5fcf75b954470c09024450fc72f3d
pradana4648 <wakwaujaim@gmail.com>
2022-07-15 15:40:57 +0700

    refactor(User): change type data key to Long

--- src/main/java/com/pradana/notes/pojo/User.java
+++ src/main/java/com/pradana/notes/pojo/User.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -11,8 +12,9 @@ import lombok.Setter;
 @Entity
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @SequenceGenerator(name = "user_generator", sequenceName = "user_seq")
+    private Long id;
 
     @Getter
     @Setter
